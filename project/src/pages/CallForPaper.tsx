ccdcda0016b83ab7183d9d600ba89bfeec496ec2
GOVIND V NAMBOOTHIRI <gvndnamboothiri@gmail.com>
2025-02-05 17:14:14 +0530

    added cmt link

--- project/src/pages/CallForPaper.tsx
+++ project/src/pages/CallForPaper.tsx
@@ -169,7 +169,7 @@ export default function CallForPaper() {
             </p>
             <div className="mt-4">
               <a
-                href="https://cmt3.research.microsoft.com/User/Login?ReturnUrl=%2F"
+                href="https://cmt3.research.microsoft.com/NCICST2025"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="inline-block bg-blue-600 text-white font-medium px-6 py-3 rounded-full shadow hover:bg-blue-700 transition"
